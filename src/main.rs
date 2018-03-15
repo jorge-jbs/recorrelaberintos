b970a50d241292b095dfb83aa300abb4728cfc9b
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-03-15 18:31:46 +0100

    Conseguir leer la imagen

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 extern crate image;
 
 use std::fs::File;
+use std::rc::Rc;
 use image::ImageDecoder;
+
+struct Node {
+    pos: (u32, u32),
+    siblings: Vec<Rc<Node>>,
+}
 
 fn main() {
     let mut decoder = image::bmp::BMPDecoder::new(File::open("laberinto-1.bmp").unwrap());
-    if let image::DecodingResult::U8(vec) = decoder.read_image().unwrap() {
-        for 
-        for p in vec {
-            print!("{:3} ", p);
+    let frames = decoder.into_frames().unwrap().next().unwrap();
+    let buf = frames.into_buffer();
+    let mut dy = 0;
+    for (x, y, pixel) in buf.enumerate_pixels() {
+        if dy != y {
+            println!("");
         }
+        dy = y;
+        print!("{:3}  ", pixel.data[0]);
     }
-    println!("{:?}", decoder.colortype());
 }
