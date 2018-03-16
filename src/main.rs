1d26f11708de58bf47021b6a3491d3d5f171fa05
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-03-16 21:13:58 +0100

    Hacer cosas... que no funcionan...

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,14 @@ impl PartialOrd for Pos {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Edge(Pos, Pos);
+
+impl Edge {
+    fn connects_to(self, pos: Pos) -> bool {
+        self.0 == pos || self.1 == pos
+    }
+}
 
 impl PartialEq for Edge {
     fn eq(&self, other: &Edge) -> bool {
@@ -69,6 +75,7 @@ impl<A: Into<Pos>, B: Into<Pos>> From<(A, B)> for Edge {
     }
 }
 
+#[derive(Clone)]
 struct Graph {
     start: Pos,
     end: Pos,
@@ -80,7 +87,13 @@ fn main() {
     for &Edge(a, b) in &graph.edges {
         println!("{:?} <-> {:?}", a, b);
     }
-    println!("{}", graph.edges.contains(&((8, 3), (8, 2)).into()));
+    println!("{}", graph.edges.len());
+    let new_graph = chain_all_edges(&graph);
+    //println!("{}", graph.edges.contains(&((3, 1), (4, 1)).into()));
+    println!("{}", new_graph.edges.len());
+    for &Edge(a, b) in &new_graph.edges {
+        println!("{:?} <-> {:?}", a, b);
+    }
 }
 
 fn read_graph() -> Graph {
@@ -117,5 +130,85 @@ fn read_graph() -> Graph {
         start: start.unwrap(),
         end: end.unwrap(),
         edges,
+    }
+}
+
+fn chain_all_edges(graph: &Graph) -> Graph {
+    let mut new_graph = graph.clone();
+    /*
+    for &e in &graph.edges {
+        for &f in &graph.edges {
+            //println!("{}", new_graph.edges.len());
+            chain_separate_edges(&mut new_graph, e, f);
+        }
+    }
+    */
+    for &e in &graph.edges {
+        chain_in_node(&mut new_graph, e.0);
+        chain_in_node(&mut new_graph, e.1);
+        //chain_separate_edges(&mut new_graph, e, f);
+    }
+    new_graph
+}
+
+fn chain_edges(graph: &Graph) -> Graph {
+    let mut new_graph = graph.clone();
+    for &Edge(a, b) in &graph.edges {
+        chain_adjacent_edges(&mut new_graph, a);
+        chain_adjacent_edges(&mut new_graph, b);
+    }
+    new_graph
+}
+
+fn chain_adjacent_edges(graph: &mut Graph, init: Pos) {
+    let north = init.y > 0 && graph.edges.contains(&(init, (init.x, init.y-1)).into());
+    let south = graph.edges.contains(&(init, (init.x, init.y+1)).into());
+    let east = graph.edges.contains(&(init, (init.x+1, init.y)).into());
+    let west = init.x > 0 && graph.edges.contains(&(init, (init.x-1, init.y)).into());
+    match (north, south, east, west) {
+        (true, true, false, false) => {
+            graph.edges.remove(&(init, (init.x, init.y-1)).into());
+            graph.edges.remove(&(init, (init.x, init.y+1)).into());
+            graph.edges.insert(((init.x, init.y-1), (init.x, init.y+1)).into());
+            //chain_separate_edges(graph, (init.x, init.y-1).into(), (init.x, init.y+1).into());
+        }
+        (false, false, true, true) => {
+            graph.edges.remove(&(init, (init.x+1, init.y)).into());
+            graph.edges.remove(&(init, (init.x-1, init.y)).into());
+            graph.edges.insert(((init.x-1, init.y), (init.x+1, init.y)).into());
+        }
+        _ => (),
+    }
+}
+
+fn chain_in_node(graph: &mut Graph, pos: Pos) {
+    let edges = graph.edges.iter().cloned().filter(|e| e.connects_to(pos)).collect::<Vec<_>>();
+    if edges.len() == 2 {
+        chain_separate_edges(graph, edges[0], edges[1]);
+    }
+}
+
+fn chain_separate_edges(graph: &mut Graph, a: Edge, b: Edge) {
+    if a == b || !graph.edges.contains(&a) || !graph.edges.contains(&b) {
+        return
+    }
+    let (left, mid, right) = {
+        if a.0 == b.0 {
+            (a.1, a.0, b.1)
+        } else if a.0 == b.1 {
+            (a.1, a.0, b.0)
+        } else if a.1 == b.0 {
+            (a.0, a.1, b.1)
+        } else if a.1 == b.1 {
+            (a.0, a.1, b.0)
+        } else {
+            return
+            //panic!("Las aristas no son adyacentes.");
+        }
+    };
+    if graph.edges.iter().filter(|e| e.connects_to(mid)).count() == 2 {
+        graph.edges.remove(&a);
+        graph.edges.remove(&b);
+        graph.edges.insert((left, right).into());
     }
 }
