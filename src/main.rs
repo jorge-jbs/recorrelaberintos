6b82465f7fa2d2e55e4c8685d63471e7379cce0a
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-03-17 18:20:25 +0100

    Use priority queue

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate image;
 
 use std::fs::File;
+use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use image::ImageDecoder;
@@ -110,47 +111,68 @@ fn read_graph() -> Graph {
 }
 
 fn distance(a: Pos, b: Pos) -> f32 {
-    ((a.x+b.x).pow(2) as f32 + (a.y+b.y).pow(2) as f32).sqrt()
+    let ax = a.x as f32;
+    let ay = a.y as f32;
+    let bx = b.x as f32;
+    let by = b.y as f32;
+    ((ax-bx).powf(2.0) + (ay-by).powf(2.0)).sqrt()
 }
 
 #[derive(PartialEq, Clone, Debug)]
 struct Node {
     pos: Pos,
-    distance: f32,
+    cost: f32,
     neighbours: [Option<Pos>; 4],
     path: Vec<Pos>,
 }
 
-fn breadth_first_search(graph: Graph) -> Vec<Pos> {
-    let node = Node {
-        pos: graph.start,
-        distance: distance(graph.start, graph.end),
-        neighbours: graph.nodes[&graph.start],
-        path: Vec::new(),
+impl Eq for Node {}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Node) -> std::cmp::Ordering {
+        other.cost.partial_cmp(&self.cost).unwrap()
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Node) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn breadth_first_search(graph: Graph) -> f32 {
+    let mut frontier: BinaryHeap<Node> = {
+        let node = Node {
+            pos: graph.start,
+            cost: 0.0,
+            neighbours: graph.nodes[&graph.start],
+            path: Vec::new(),
+        };
+        if node.pos == graph.end { return 0.0 }
+        let mut frontier: BinaryHeap<Node> = BinaryHeap::new();
+        frontier.push(node);
+        frontier
     };
-    if node.pos == graph.end { return Vec::new() }
-    let mut frontier: Vec<Node> = vec![node];
     let mut explored: HashSet<Pos> = HashSet::new();
     loop {
-        //println!("{:?}", frontier.iter().map(|x| x.pos).collect::<Vec<_>>());
         if frontier.is_empty() { panic!("Fallé. ¡Imposible!") }
-        let nodo = frontier.pop().unwrap();
-        explored.insert(nodo.pos);
-        for neighbour in &nodo.neighbours {
+        let node = frontier.pop().unwrap();
+        explored.insert(node.pos);
+        for neighbour in &node.neighbours {
             if let &Some(neighbour) = neighbour {
-                let mut new_path = nodo.path.clone();
+                let mut new_path = node.path.clone();
                 new_path.push(neighbour);
                 let child = Node {
                     pos: neighbour,
-                    distance: distance(nodo.pos, neighbour),
+                    cost: node.cost + distance(node.pos, neighbour),
                     neighbours: graph.nodes[&neighbour],
                     path: new_path,
                 };
                 if !explored.contains(&neighbour) && frontier.iter().find(|&x| x.clone() == child).is_none() {
                     if neighbour == graph.end {
-                        return child.path
+                        return child.cost
                     }
-                    frontier.insert(0, child);
+                    frontier.push(child);
                 }
             }
         }
